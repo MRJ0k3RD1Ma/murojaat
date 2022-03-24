89354b470be0b3b38fdc2e5b0ce2487617d5cad3
Dilmurod Allabergenov <mr.j0k3r.d1ma@gmail.com>
2022-03-24 19:26:08 +0500

    dfg

---
+++ common/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=admin_murojaat',
+            'username' => 'admin_murojaat',
+            'password' => '2Z76cbLTTm',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
